95dd6a1bfc73bc80a31b8d9cbf89e7fa1f11a659
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-04-15 00:07:50 -0400

    User CRUD endpoints add

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code away!
+const express = require('express');
+const userRouter = require('./users/userRouter.js');
+
+const server = express();
+
+server.use(express.json());
+const port = 9090;
+server.use('/api/users/',userRouter);
+
+server.listen(port,() => {
+    console.log(`Server running at http://localhost:${port}`)
+})
+
+
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,159 @@
 const express = require('express');
+const userDB = require('./userDb.js');
+const postDB = require('../posts/postDb.js')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+// Create New User
+router.post('/', validateUser(), (req, res) => {  
+        
+    userDB.insert(req.body) 
+      .then((user) => {
+        res.status(201).json(user)
+      })
+      .catch((error) => {
+        next(error)
+      })   
+
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+
+// Create New Post By User ID
+router.post('/:id/posts', validatePost(), validateUserId() ,(req, res) => {
+  
+     const user_id = req.params.id 
+    // const { text } = req.body;
+    // const { id: user_id } = req.params;
+    // insert({ text, user_id })
+    req.body = {...req.body, "user_id": user_id}
+    //console.log(req.body);
+    postDB.insert(req.body)
+      .then((post) => {
+        res.status(201).json(post)
+      })
+      .catch((error) => {
+        next(error)
+      })    
+        
 });
 
+
+// Get Users
 router.get('/', (req, res) => {
-  // do your magic!
+    
+    userDB.get()
+      .then((users) => {
+        res.status(200).json(users);
+      })
+      .catch((error) => {
+        next(error)
+      })
+  
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+// Get User By ID
+router.get('/:id', validateUser(), (req, res) => {    
+    res.status(200).json(req.user)    
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+// Get User Posts By User ID
+router.get('/:id/posts', validateUserId(), (req, res) => {
+  
+    userDB.getUserPosts(req.params.id)
+      .then((posts) => {
+        res.json(posts)
+      })
+      .catch((error) => {
+        next(error)
+      })
+
 });
 
+
+// Delete User By ID
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  
+    userDB.remove(req.params.id)
+      .then((users) => {
+        res.json(users)
+      })
+      .catch((error) => {
+        next(error)
+      })
+
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+
+// Update User By ID
+router.put('/:id', validateUser(), validateUserId(), (req, res) => {
+ 
+    userDB.update(req.params.id,req.body)
+      .then((user) => {
+        res.status(200).json(user)
+      })
+      .catch((error) => {
+        next(error)
+      })
+   
+
 });
+
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  
+    return (req, res, next) => {
+
+      userDB.getById(req.params.id)
+        .then((user) => {
+          if(user) {
+              req.user = user
+              next()
+          } else {
+            res.status(404).json({message: "The user with the specified ID does not exist"})
+            res.json(user); 
+          }
+          
+        })
+        .catch((error) => {
+            next(error)
+        })
+
+    }  
+  
+  
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+
+    return (req, res, next) => {
+
+        if(!req.body.name) {
+          res.status(400).json({
+            message: "Please provide name for the user."
+          })
+        } else {
+          next()
+        }
+    }   
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+    
+     return (req, res, next) => {
+         
+        if(!req.body.text) {
+          res.status(400).json({
+            message: "Please provide name for the post."
+          })
+        } else {
+          next()
+        }
+
+
+     }
+  
 }
 
 module.exports = router;
