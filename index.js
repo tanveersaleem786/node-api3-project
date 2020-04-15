84dac8d4dc0062e9ed5fb32e1591d2af7eaed4a7
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-04-15 18:32:10 -0400

    HIT MVP

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // code away!
 const express = require('express');
+const  logger = require('./middleware/logger.js')
 const userRouter = require('./users/userRouter.js');
 
 const server = express();
@@ -7,6 +8,20 @@ const server = express();
 server.use(express.json());
 const port = 9090;
 server.use('/api/users/',userRouter);
+server.use(logger({format : "long"}))
+
+server.use((req, res) => {
+	res.status(404).json({
+		message: "Route was not found",
+	})
+})
+
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+        error: "Something went wrong"
+    })
+})
 
 server.listen(port,() => {
     console.log(`Server running at http://localhost:${port}`)
